9f4e0b1f8e20030ce96b0cfb40673e7612426233
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-07 15:26:48 +0300

    shopItem model

---
+++ models/shopItem.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+// reikia apibrezti kokio tipo duomenys bus saugomi DB
+
+// { title: string, body: string, .. }
+
+const shopItemSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    salePrice: {
+      type: Number,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [Number],
+      required: true,
+    },
+    sku: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'shopCategory',
+    },
+  },
+  { timestamps: true } /// adds timestamps
+);
+
+// exportuoti naujai sukurta objekta pagal sia schema
+//                           turetu buti vienaskai musu kolecijos pav.
+const ShopItem = mongoose.model('shopItem', shopItemSchema);
+
+module.exports = ShopItem;
