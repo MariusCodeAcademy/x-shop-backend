a9b49561f3f8461374918c4557a218a832f2eb14
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-07 14:20:56 +0300

    shop category model

---
+++ models/shopCategory.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+// reikia apibrezti kokio tipo duomenys bus saugomi DB
+
+// { title: string, body: string, .. }
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } /// adds timestamps
+);
+
+// exportuoti naujai sukurta objekta pagal sia schema
+//                           turetu buti vienaskai musu kolecijos pav.
+const Post = mongoose.model('post', postSchema);
+
+module.exports = Post;
