e5498f41646ccb21985b352d657159e671d2291f
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-08 08:30:40 +0300

    add itemRoutes, test post item

--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -3,6 +3,10 @@ const ShopItem = require('../models/shopItem');
 const router = express.Router();
 
 // sukurti nauja item
+router.post('/api/shop/items/new', (req, res) => {
+  console.log(req.body);
+  res.json('You are about to add item');
+});
 
 // gauti visus items
 
