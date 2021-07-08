f4f6497b1e200ba5525988784cfb42004a74d4e6
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-08 09:32:21 +0300

    3rd and 4th items

--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -7,18 +7,19 @@ router.post('/api/shop/items/new', (req, res) => {
   console.log(req.body);
 
   const newItemData = {
-    title: 'Stealth Bomber Jacket',
-    price: 1599.95,
-    image: 'acc_jacket_01_',
-    color: 'navy',
+    title: 'Autum best',
+    price: 299.99,
+    image: 'foot_autum_01_',
+    color: 'green',
+    size: 'normal',
     sizeQty: [
-      { size: 'small', quantity: 5 },
-      { size: 'medium', quantity: 5 },
-      { size: 'large', quantity: 5 },
+      { size: 'small', quantity: 3 },
+      { size: 'medium', quantity: 3 },
+      { size: 'large', quantity: 3 },
     ],
-    images: [1, 2, 3],
-    sku: 'jacket_01',
-    category: '60e593cabf4b829b3784a7b5',
+    images: [1, 2, 3, 4],
+    sku: 'autum_01',
+    category: '60e593a6bf4b829b3784a7ad',
   };
   const newItem = new ShopItem(newItemData);
 
