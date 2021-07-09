2de17ecb630fc4104c199be628da63c21393ec6a
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-09 08:42:55 +0300

    get all user route works

--- Routes/shopUserRoutes.js
+++ Routes/shopUserRoutes.js
@@ -2,18 +2,17 @@ const express = require('express');
 const ShopUser = require('../models/shopUser');
 const router = express.Router();
 
-router.get('/api/shop/shopusers/', (req, res) => {
-  res.json('there should be all users here');
+router.get('/api/shop/shopusers/', async (req, res) => {
+  try {
+    const users = await ShopUser.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // sukurti useri
 router.post('/api/shop/shopusers/new', async (req, res) => {
-  const newUserData = {
-    name: 'Bob Stoned',
-    email: 'Bob@Stoned.com',
-    password: 'secret123',
-  };
-
   const newUser = new ShopUser(req.body);
   try {
     const result = await newUser.save();
