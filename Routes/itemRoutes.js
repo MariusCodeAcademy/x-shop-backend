21c37251f7bfe948c025da24aecae51c1e1d3d45
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-14 08:52:17 +0300

    stock with cat ids to change

--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -7,19 +7,15 @@ router.post('/api/shop/items/new', (req, res) => {
   console.log(req.body);
 
   const newItemData = {
-    title: 'Autum best',
+    title: 'Autum best shoes',
     price: 299.99,
     image: 'foot_autum_01_',
     color: 'green',
     size: 'normal',
-    sizeQty: [
-      { size: 'small', quantity: 3 },
-      { size: 'medium', quantity: 3 },
-      { size: 'large', quantity: 3 },
-    ],
+    quantity: 3,
     images: [1, 2, 3, 4],
     sku: 'autum_01',
-    category: '60e593a6bf4b829b3784a7ad',
+    category: '60e593bbbf4b829b3784a7b1',
   };
   const newItem = new ShopItem(newItemData);
 
